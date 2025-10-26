527338d93cb7e4988d6586c8d48d4ba400fddc18
Sumit Kumar <smtkur31@gmail.com>
2025-10-26 16:40:19 +0530

    background_jobs

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -6,10 +6,7 @@ export default async function LoginPage() {
   await requireUnauth();
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-4xl">
-        <SignupForm />
-      </div>
-    </div>
+    <SignupForm />
+
   );
 }
